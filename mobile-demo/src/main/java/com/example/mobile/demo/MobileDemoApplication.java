0efa0232224d45487383f2e596b65ac4012dee8d
wuy <1311695042@qq.com>
2021-12-31 22:13:32 +0800

    + 增加移动端后端demo

---
+++ mobile-demo/src/main/java/com/example/mobile/demo/MobileDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mobile.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileDemoApplication.class, args);
+    }
+
+}
