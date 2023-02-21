ea5e52e7c3e3c14288ee597c77e71d3e351f0f9a
wind <1103642893@qq.com>
2023-02-21 22:29:06 +0800

    * 重构系统配置

--- demo/src/main/java/com/example/demo/config/SpringMvcConfig.java
+++ demo/src/main/java/com/example/demo/config/SpringMvcConfig.java
@@ -1,16 +1,13 @@
 package com.example.demo.config;
 
-import com.diboot.iam.config.SystemConfigInjection;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import java.util.Collections;
-
 /**
  * Spring 相关配置
+ *
  * @author www.dibo.ltd
  * @version v1.0
  * @date 2020/10/23
@@ -20,11 +17,5 @@ import java.util.Collections;
 @ComponentScan(basePackages = {"com.example"})
 @MapperScan(basePackages = {"com.example.demo.mapper"})
 public class SpringMvcConfig {
-    /**
-     * 注入系统配置枚举类
-     */
-    @Bean
-    public SystemConfigInjection systemConfig() {
-        return () -> Collections.singletonList(EmailConfig.class);
-    }
+
 }
--- demo/src/main/java/com/example/demo/controller/system/SystemConfigController.java
+++ demo/src/main/java/com/example/demo/controller/system/SystemConfigController.java
@@ -1,22 +1,22 @@
 package com.example.demo.controller.system;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.diboot.core.controller.BaseCrudRestController;
 import com.diboot.core.util.S;
+import com.diboot.core.util.V;
 import com.diboot.core.vo.JsonResult;
-import com.diboot.core.vo.LabelValue;
 import com.diboot.iam.annotation.BindPermission;
 import com.diboot.iam.annotation.Log;
 import com.diboot.iam.annotation.OperationCons;
-import com.diboot.iam.config.SystemConfigType;
 import com.diboot.iam.entity.SystemConfig;
 import com.diboot.iam.service.SystemConfigService;
-import com.diboot.iam.vo.SystemConfigVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
+import javax.validation.Valid;
 import java.util.List;
-import java.util.Map;
 
 /**
  * 系统配置Controller
@@ -41,91 +41,140 @@ public class SystemConfigController extends BaseCrudRestController<SystemConfig>
      */
     @Log(operation = OperationCons.LABEL_LIST)
     @BindPermission(name = OperationCons.LABEL_LIST, code = OperationCons.CODE_READ)
-    @GetMapping("/type-list")
-    public JsonResult<List<LabelValue>> getTypeListMapping() {
-        return JsonResult.OK(systemConfigService.getTypeList());
+    @GetMapping("/category")
+    public JsonResult<List<String>> getCategoryListMapping() {
+        return JsonResult.OK(systemConfigService.getValuesOfField(Wrappers.lambdaQuery(), SystemConfig::getCategory));
+    }
+
+    /**
+     * 查询ViewObject的分页数据
+     * <p>
+     * url请求参数示例: ?field=abc&pageSize=20&pageIndex=1&orderBy=id
+     * </p>
+     */
+    @Log(operation = OperationCons.LABEL_LIST)
+    @BindPermission(name = OperationCons.LABEL_LIST, code = OperationCons.CODE_READ)
+    @GetMapping
+    public JsonResult<List<SystemConfig>> getViewObjectListMapping(SystemConfig entity) throws Exception {
+        QueryWrapper<SystemConfig> queryWrapper = super.buildQueryWrapperByQueryParams(entity);
+        if (V.isEmpty(entity.getCategory())) {
+            queryWrapper.lambda().and(query -> query.eq(SystemConfig::getCategory, S.EMPTY).or().isNull(SystemConfig::getCategory));
+        }
+        return JsonResult.OK(systemConfigService.getEntityList(queryWrapper, null));
     }
 
     /**
      * 获取指定类型的系统配置信息
      *
-     * @param type 类型
+     * @param id 类型
      * @return
      */
     @Log(operation = OperationCons.LABEL_DETAIL)
     @BindPermission(name = OperationCons.LABEL_DETAIL, code = OperationCons.CODE_READ)
-    @GetMapping("/{type}")
-    public JsonResult<List<SystemConfigVO>> getConfigByTypeMapping(@PathVariable String type) {
-        return JsonResult.OK(systemConfigService.getConfigByType(type));
+    @GetMapping("/{id}")
+    public JsonResult<SystemConfig> getConfigByTypeMapping(@PathVariable String id) {
+        return JsonResult.OK(systemConfigService.getEntity(id));
     }
 
     /**
-     * 更新系统配置
+     * 创建资源对象
      *
-     * @param systemConfig
-     * @return
+     * @param entity
+     * @return JsonResult
+     * @throws Exception
+     */
+    @Log(operation = OperationCons.LABEL_CREATE)
+    @BindPermission(name = OperationCons.LABEL_CREATE, code = OperationCons.CODE_WRITE)
+    @PostMapping
+    public JsonResult<?> createEntityMapping(@Valid @RequestBody SystemConfig entity) throws Exception {
+        return super.createEntity(entity);
+    }
+
+    /**
+     * 根据ID更新资源对象
+     *
+     * @param entity
+     * @return JsonResult
+     * @throws Exception
      */
     @Log(operation = OperationCons.LABEL_UPDATE)
     @BindPermission(name = OperationCons.LABEL_UPDATE, code = OperationCons.CODE_WRITE)
-    @PostMapping
-    public JsonResult<?> updateConfigMapping(@RequestBody SystemConfig systemConfig) {
-        return new JsonResult<>(systemConfigService.createOrUpdateEntity(systemConfig));
+    @PutMapping("/{id}")
+    public JsonResult<?> updateEntityMapping(@PathVariable("id") String id, @Valid @RequestBody SystemConfig entity) throws Exception {
+        return super.updateEntity(id, entity);
     }
 
     /**
-     * 重置指定类型或属性的系统配置
+     * 批量更新接口
      *
-     * @param type 类型
-     * @param prop 属性（为空重置整个类型）
+     * @param list
      * @return
+     * @throws Exception
      */
-    @Log(operation = "重置")
-    @BindPermission(name = "重置", code = OperationCons.CODE_WRITE)
-    @DeleteMapping({"/{type}/{prop}", "/{type}"})
-    public JsonResult<?> deleteTypeMapping(@PathVariable String type, @PathVariable(required = false) String prop) {
-        systemConfigService.deleteByTypeAndProp(type, prop);
-        return JsonResult.OK();
+    @Log(operation = OperationCons.LABEL_UPDATE)
+    @BindPermission(name = OperationCons.LABEL_UPDATE, code = OperationCons.CODE_WRITE)
+    @PutMapping
+    public JsonResult<?> updateEntityListMapping(@Valid @RequestBody List<SystemConfig> list) throws Exception {
+        return new JsonResult<>(systemConfigService.updateEntities(list));
     }
 
     /**
-     * 系统配置测试
+     * 根据id删除资源对象
      *
-     * @param type 类型
-     * @param data 数据
+     * @param id
      * @return
+     * @throws Exception
      */
-    @Log(operation = "测试")
-    @BindPermission(name = "测试", code = OperationCons.CODE_WRITE)
-    @PostMapping("/{type}")
-    public JsonResult<?> configTestMapping(@PathVariable String type, @RequestBody Map<String, Object> data) {
-        systemConfigService.configTest(type, data);
-        return JsonResult.OK();
+    @Log(operation = OperationCons.LABEL_DELETE)
+    @BindPermission(name = OperationCons.LABEL_DELETE, code = OperationCons.CODE_WRITE)
+    @DeleteMapping("/{id}")
+    public JsonResult<?> deleteEntityMapping(@PathVariable("id") String id) throws Exception {
+        return super.deleteEntity(id);
     }
 
     /**
      * 获取配置值 用于前端获取配置值
      *
-     * @param type 配置类型
-     * @param prop 属性（为空重置整个类型）
+     * @param category 配置类型
+     * @param propKey 属性（为空重置整个类型）
      * @return 配置值
      */
     @Log(operation = "获取配置值")
-    @GetMapping({"/value/{type}/{prop}", "/value/{type}"})
-    public JsonResult<?> getConfigMapping(@PathVariable String type, @PathVariable(required = false) String prop) {
+    @GetMapping("/value")
+    public JsonResult<?> getConfigMapping(@RequestParam(required = false) String category, @RequestParam(required = false) String propKey) {
+        if (S.isEmpty(propKey)) {
+            return JsonResult.OK(systemConfigService.getConfigMapByCategory(category));
+        }
+        return JsonResult.OK(systemConfigService.findConfigValue(category, propKey));
+    }
 
-        List<Enum<? extends SystemConfigType>> configTypeItems = systemConfigService.getConfigItemsMap().getOrDefault(type, Collections.emptyList());
-        if (configTypeItems.isEmpty()) {
-            return JsonResult.FAIL_VALIDATION("配置类型：" + type + " 不存在");
+    /**
+     * 验证属性名是否重复
+     *
+     * @param id
+     * @param propKey
+     * @param category
+     * @return
+     */
+    @GetMapping("/check-prop-key-duplicate")
+    public JsonResult<?> checkPropKeyDuplicate(@RequestParam(required = false) String id, @RequestParam String propKey,
+                                               @RequestParam(required = false) String category) {
+
+        if (V.isEmpty(propKey)) {
+            return JsonResult.OK();
         }
-        if (S.isEmpty(prop)) {
-            return JsonResult.OK(SystemConfigType.values(configTypeItems.get(0).getClass()).getMap());
+        LambdaQueryWrapper<SystemConfig> wrapper = Wrappers.<SystemConfig>lambdaQuery().eq(SystemConfig::getPropKey, propKey);
+        // 如果id存在，那么需要排除当前id进行查询
+        wrapper.ne(V.notEmpty(id), SystemConfig::getId, id);
+        if (V.isEmpty(category)) {
+            wrapper.and(query -> query.eq(SystemConfig::getCategory, S.EMPTY).or().isNull(SystemConfig::getCategory));
+        } else {
+            wrapper.eq(SystemConfig::getCategory, category);
         }
-        for (Enum<? extends SystemConfigType> configTypeItem : configTypeItems) {
-            if (configTypeItem.name().equals(prop)) {
-                return JsonResult.OK(((SystemConfigType) configTypeItem).getValue());
-            }
+        if (systemConfigService.exists(wrapper)) {
+            return JsonResult.FAIL_VALIDATION((V.isEmpty(category) ? "" : "类别[" + category + "]中") + "属性名[" + propKey + "]已存在");
         }
-        return JsonResult.FAIL_VALIDATION("配置类型：" + type + " 属性：" + prop + " 不存在");
+        return JsonResult.OK();
     }
 
 }
