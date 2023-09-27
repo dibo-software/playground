b4473be2318a865672b59275556284a6f9df956c
mazc <mazc@dibo.ltd>
2023-09-27 16:01:25 +0800

    * 更新依赖至2.10.0

--- demo/src/main/java/com/example/demo/handler/AutoFillMetaObjectHandler.java
+++ demo/src/main/java/com/example/demo/handler/AutoFillMetaObjectHandler.java
@@ -5,26 +5,24 @@ import com.diboot.core.config.Cons;
 import com.diboot.iam.util.IamSecurityUtils;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
 import java.util.Date;
 
 /**
- * 字段自动填充策略处理类
- *
- * @author MyName
- * @version 1.0
- * @date 2022-12-30
- * Copyright © MyCompany
- */
+* 字段自动填充策略处理类
+* @author MyName
+* @version 1.0
+* @date 2023-09-27
+* Copyright © MyCompany
+*/
 @Component
-public class AutoFillMetaObjectHandler implements MetaObjectHandler {
-
+public class AutoFillMetaObjectHandler implements MetaObjectHandler{
+		
     @Override
     public void insertFill(MetaObject metaObject) {
+        // 创建数据时，创建时间、创建人、更新时间自动填充
         this.strictInsertFill(metaObject, Cons.FieldName.createTime.name(), Date::new, Date.class);
-        this.strictInsertFill(metaObject, Cons.FieldName.createBy.name(), IamSecurityUtils::getCurrentUserId, Long.class);
         this.strictInsertFill(metaObject, Cons.FieldName.updateTime.name(), Date::new, Date.class);
+        this.strictInsertFill(metaObject, Cons.FieldName.createBy.name(), IamSecurityUtils::getCurrentUserId, Long.class);
     }
 
     @Override
--- demo/src/main/java/com/example/demo/handler/GeneralExceptionHandler.java
+++ demo/src/main/java/com/example/demo/handler/GeneralExceptionHandler.java
@@ -1,16 +1,16 @@
 package com.example.demo.handler;
 
 import com.diboot.core.handler.DefaultExceptionHandler;
-import lombok.extern.slf4j.Slf4j;
+ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 /**
- * 通用异常处理类
- * @author MyName
- * @version 1.0
- * @date 2022-12-30
- * Copyright © MyCompany
- */
+* 通用异常处理类
+* @author MyName
+* @version 1.0
+* @date 2023-09-27
+* Copyright © MyCompany
+*/
 @ControllerAdvice
 @Slf4j
 public class GeneralExceptionHandler extends DefaultExceptionHandler{
