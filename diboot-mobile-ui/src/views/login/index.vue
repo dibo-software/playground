aa1780e868a47fd6e3624e24f792d693e031679d
zhangxiong <zx164931202>
2024-05-23 10:38:47 +0800

    移动端用户名密码输入框添加高度

--- diboot-mobile-ui/src/views/login/index.vue
+++ diboot-mobile-ui/src/views/login/index.vue
@@ -49,12 +49,10 @@ const onSubmit = () => {
       loading.value = false
     })
 }
-
 </script>
 
 <template>
   <div class="content">
-
     <van-form @submit="onSubmit">
       <h2 style="text-align: center">Diboot 移动端 v3.0</h2>
       <van-cell-group inset>
@@ -90,7 +88,7 @@ const onSubmit = () => {
           </template>
         </van-field>
       </van-cell-group>
-      <div style="margin: 16px">
+      <div style="margin: 24px 16px">
         <van-button round block type="primary" native-type="submit"> 提交 </van-button>
       </div>
     </van-form>
@@ -104,35 +102,85 @@ const onSubmit = () => {
   justify-content: center;
 
   background-image: radial-gradient(closest-side, rgb(120, 142, 225), rgba(248, 192, 147, 0)),
-  radial-gradient(closest-side, rgb(168, 178, 220), rgba(170, 142, 245, 0)),
-  radial-gradient(closest-side, rgb(143, 173, 210), rgba(235, 105, 78, 0)),
-  radial-gradient(closest-side, rgb(129, 199, 211), rgba(243, 11, 164, 0)),
-  radial-gradient(closest-side, rgb(137, 196, 148), rgba(254, 234, 131, 0));
-  background-size: 130vmax 130vmax, 80vmax 80vmax, 90vmax 90vmax, 110vmax 110vmax, 90vmax 9vmax;
-  background-position: -80vmax -80vmax, 60vmax -30vmax, 10vmax 10vmax, -30vmax -10vmax, 50vmax 50vmax;
+    radial-gradient(closest-side, rgb(168, 178, 220), rgba(170, 142, 245, 0)),
+    radial-gradient(closest-side, rgb(143, 173, 210), rgba(235, 105, 78, 0)),
+    radial-gradient(closest-side, rgb(129, 199, 211), rgba(243, 11, 164, 0)),
+    radial-gradient(closest-side, rgb(137, 196, 148), rgba(254, 234, 131, 0));
+  background-size:
+    130vmax 130vmax,
+    80vmax 80vmax,
+    90vmax 90vmax,
+    110vmax 110vmax,
+    90vmax 9vmax;
+  background-position:
+    -80vmax -80vmax,
+    60vmax -30vmax,
+    10vmax 10vmax,
+    -30vmax -10vmax,
+    50vmax 50vmax;
   background-repeat: no-repeat;
   animation: 12s movement linear infinite;
 
   @keyframes movement {
     0%,
     100% {
-      background-size: 130vmax 130vmax, 80vmax 80vmax, 90vmax 90vmax, 110vmax 110vmax, 90vmax 90vmax;
-      background-position: -80vmax -80vmax, 60vmax -30vmax, 10vmax 10vmax, -30vmax -10vmax, 50vmax 50vmax;
+      background-size:
+        130vmax 130vmax,
+        80vmax 80vmax,
+        90vmax 90vmax,
+        110vmax 110vmax,
+        90vmax 90vmax;
+      background-position:
+        -80vmax -80vmax,
+        60vmax -30vmax,
+        10vmax 10vmax,
+        -30vmax -10vmax,
+        50vmax 50vmax;
     }
 
     25% {
-      background-size: 100vmax 100vmax, 90vmax 90vmax, 100vmax 100vmax, 90vmax 90vmax, 60vmax 60vmax;
-      background-position: -60vmax -90vmax, 50vmax -40vmax, 0vmax -20vmax, -40vmax -20vmax, 40vmax 60vmax;
+      background-size:
+        100vmax 100vmax,
+        90vmax 90vmax,
+        100vmax 100vmax,
+        90vmax 90vmax,
+        60vmax 60vmax;
+      background-position:
+        -60vmax -90vmax,
+        50vmax -40vmax,
+        0vmax -20vmax,
+        -40vmax -20vmax,
+        40vmax 60vmax;
     }
 
     50% {
-      background-size: 80vmax 80vmax, 110vmax 110vmax, 80vmax 80vmax, 60vmax 60vmax, 80vmax 80vmax;
-      background-position: -50vmax -70vmax, 40vmax -30vmax, 10vmax 0vmax, 20vmax 10vmax, 30vmax 70vmax;
+      background-size:
+        80vmax 80vmax,
+        110vmax 110vmax,
+        80vmax 80vmax,
+        60vmax 60vmax,
+        80vmax 80vmax;
+      background-position:
+        -50vmax -70vmax,
+        40vmax -30vmax,
+        10vmax 0vmax,
+        20vmax 10vmax,
+        30vmax 70vmax;
     }
 
     75% {
-      background-size: 90vmax 90vmax, 90vmax 90vmax, 100vmax 100vmax, 90vmax 90vmax, 70vmax 70vmax;
-      background-position: -50vmax -40vmax, 50vmax -30vmax, 20vmax 0vmax, -10vmax 19vmax, 40vmax 60vmax;
+      background-size:
+        90vmax 90vmax,
+        90vmax 90vmax,
+        100vmax 100vmax,
+        90vmax 90vmax,
+        70vmax 70vmax;
+      background-position:
+        -50vmax -40vmax,
+        50vmax -30vmax,
+        20vmax 0vmax,
+        -10vmax 19vmax,
+        40vmax 60vmax;
     }
   }
   .van-radio--horizontal {
@@ -143,6 +191,9 @@ const onSubmit = () => {
   }
   .van-radio__label {
     margin-left: 0;
+  }
+  .van-cell {
+    padding: var(--van-padding-lg);
   }
 }
 </style>
